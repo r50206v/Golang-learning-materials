d6ad8c3ffb20f668cba7afffb2356325bcbc594b
richard <r50206v@gmail.com>
2021-09-29 12:01:23 -0400

    [FEATURE] hello world

---
+++ hello/hello.go
@@ -0,0 +1,26 @@
+package main 
+/*
+	This line is called a package declaration and every Go program
+	starts with one. The package declaration informs the compiler 
+	whether to create an executable or library. 
+*/
+
+// import "fmt"
+// import "time"
+import (
+	"fmt" 
+	t "time"
+)
+/*
+	package name is enclosed with double quotes "
+*/
+
+func main(){
+	fmt.Printf("hello, world!\n")
+	fmt.Printf(t.Now())
+}
+/*
+	a main function is special, a file that has a 
+	package main declaration will automatically run the main function!
+*/
+
